59cee6ba0b9ea6d0b03fd8e9023d8bd94bed1a89
Mike Jed <jedmike@gmail.com>
2020-02-22 20:30:07 -0500

    big push

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friendsData = [{
+        name: "Mike Tyson",
+        photo: "https://cdn.self-made.io/wp-content/uploads/2017/07/mike-tyson-807x1334.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Marilyn Monroe",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRajSigZWnchbT9lWvhMj96pNu6ggT5BoWQcUveGjQ_I_bMsGh",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Stormy Daniels",
+        photo:"https://daily-voice-res.cloudinary.com/image/upload/c_fill,dpr_auto,f_auto,q_auto:eco,w_640/stormydaniels1-jpg-0ed30cd1-stormydaniels1-jpg_e7xkx2",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Marylyn Chambers",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjHV28uFIb3uTLjvINUehfQFPz89tXSNy2-DA75zR35vM7LDyT",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Brad Pitt",
+        photo: "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Annie Potts",
+        photo: "https://vignette.wikia.nocookie.net/thefosters/images/b/b3/Annie_potts_ghostbusters_old_18ca8jc-18ca84.jpg/revision/latest/top-crop/width/360/height/450?cb=20131214025059",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+]
+
+// export the array
+module.exports = friendsData;
